0ef9977c4278ede18a77f568f37f3b37739829ba
NGHIA-17-iso <trongnghia314314@gmail.com>
2025-05-07 14:33:26 +0700

    yhrr,r

--- process_login.php
+++ process_login.php
@@ -16,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
         exit;
     } else {
+        //hekloa
         header("Location: login.php?error=Invalid credentials");
         exit;
     }
